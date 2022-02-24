bd1d9b837ce96429acf2d6f3c1e287a3f0cbe301
jungho shin <wjdghtls11@gmail.com>
2022-02-24 18:54:17 +0900

    Add: repository 패턴으로 나눔

---
+++ cat-info-community/src/cats/cats.repository.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable prettier/prettier */
+import { HttpException } from '@nestjs/common';
+import { Cat } from './cats.schema';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CatRequestDto } from './dto/cats.request.dto';
+
+@Injectable()
+export class CatsRepository {
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+
+  async existByEmail(email: string): Promise<boolean> {
+      const result = await this.catModel.exists({ email });
+      if(result){ // 타입에러나서 result를 boolean으로 바꿈
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+  async create(cat :CatRequestDto): Promise<Cat> {
+    return await this.catModel.create(cat)
+  }
+}
--- cat-info-community/src/cats/cats.service.ts
+++ cat-info-community/src/cats/cats.service.ts
@@ -1,3 +1,4 @@
+import { CatsRepository } from './cats.repository';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -7,11 +8,11 @@ import { CatRequestDto } from './dto/cats.request.dto';
 
 @Injectable()
 export class CatsService {
-  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+  constructor(private readonly catsRepository: CatsRepository) {}
 
   async signUp(body: CatRequestDto) {
     const { email, name, password } = body;
-    const isCatExist = await this.catModel.exists({ email });
+    const isCatExist = await this.catsRepository.existByEmail(email);
 
     if (isCatExist) {
       throw new UnauthorizedException('해당하는 고양이는 이미 존재합니다.');
@@ -19,7 +20,7 @@ export class CatsService {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const cat = await this.catModel.create({
+    const cat = await this.catsRepository.create({
       email,
       name,
       password: hashedPassword,
